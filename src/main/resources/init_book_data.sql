9b283869fd977f461c73eec9623cd9c769144ff6
bow <sayamon.sittiprom@gmail.com>
2021-04-26 21:19:59 +0700

    add sql file

---
+++ src/main/resources/init_book_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO book_store.book (name,author,price) VALUES
+	 ('Before We Were Yours: A Novel','Lisa Wingat',340.00),
+	 ('When Never Comes','Barbara Davis',179.00),
+	 ('Giraffes Cant Dance','Giles Andreae Guy Parker-Rees',200.50),
+	 ('The Great Alone: A Novel Kristin Hannah','Kristin Hannah',495.00),
+	 ('An American Princess: The Many Lives of Allene Tew','Annejet van der Zijl, Michele Hutchison',149.00),
+	 ('Medical Medium Life-Changing Foods','Anthony William',929.70),
+	 ('Vegan 100','Gaz Oakley',897.96),
+	 ('Have You Filled A Bucket Today?','Carol McCloud',290.06),
+	 ('The Very Hungry Caterpillar','Eric Carle',208.51),
+	 ('The Hate U Give','Angie Thoma',319.16),
+	 ('The Alice Network','Kate Quinn',393.22),
+	 ('Harry Potter - A History of Magic','British Library',1379.78);
